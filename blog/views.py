55002bae11c22df1fb69e8b5d353d757a8c04888
Milena <milena.marcinik@gmail.com>
2021-02-13 22:01:40 +0100

    tutorial web app - part 5

--- blog/views.py
+++ blog/views.py
@@ -1,25 +1,10 @@
 from django.shortcuts import render
-
-
-posts = [
-    {
-        'author': 'Milena',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'February 13, 2021'
-    },
-    {
-        'author': 'Jane Doe',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'February 14, 2021'
-    },
-]
+from .models import Post
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request=request, template_name='blog/home.html', context=context)
 
